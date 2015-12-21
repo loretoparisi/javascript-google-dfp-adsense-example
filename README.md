baed2d5e4c567884ae50d7752e4a96da5caa60d1
Loreto Parisi <loretoparisi@gmail.com>
2015-12-21 19:02:00 +0100

    Dynamic loading of ads

--- README.md
+++ README.md
@@ -3,29 +3,32 @@
 ######@author loreto at musixmatch dot com
 
 Example of usage
+
+Supposed to have a html container likelihood
+
+```html
+<div id="dfp_ads_container"></div>
+```
+
 ```javascript
 // Inject DFP Script
     AdsScrambler.injectDFP();
     // Inject Adsense Script
     AdsScrambler.injectAdsense();
-    
-    // DFP Containers
-    var containersList = [
-        'div-gpt-ad-123456789-1' // example
-        'div-gpt-ad-123456789-2' // example
-    ];
-    
+
     // DFP Slots
     var slotList =  [
           {
             'id' : '/1234678/leaderboard_728_90',
             'size' :  [728, 90],
-            'container' : 'div-gpt-ad-123456789-1' // example
+            'container' : 'div-gpt-ad-123456789-1', // example
+            'containerid' : 'dfp_ads_container'
           },
           {
             'id' : '/12345678/mrect',
             'size' :  [336, 280],
-            'container' : 'div-gpt-ad-123456789-2' // example
+            'container' : 'div-gpt-ad-123456789-2', // example
+            'containerid' : 'dfp_ads_container'
           }
        ];
     // DFP options
@@ -33,9 +36,17 @@ Example of usage
          'backfill' : true,
          'collapse' : true
     };
-    
-    // Initialize the fuck up
-    adsScrambler = new AdsScrambler(containersList, slotList, options);
-      
-    // warm it up
-    adsScrambler.push();
+
+    // Inject DFP Script
+    AdsScrambler.injectDFP();
+    // Inject Adsense Script
+    AdsScrambler.injectAdsense();
+
+    // Initialize the Ads Scrambler
+    adsScrambler = new AdsScrambler(slotList, options);
+
+    // Execute setup async when the page is ready
+    AdsScrambler.run(function() {
+      adsScrambler.pushAllTags();
+      adsScrambler.push();  
+     });
--- js/adsscrambler.js
+++ js/adsscrambler.js
@@ -1,7 +1,7 @@
 /* global adsScrambler */
 /**
  *  Ads Scrambler
- * @author: loreto at musixmatch dot com
+ * @author: loreto at musixmatch dot com (Loreto Parisi)
  * @copyright 2015 Musixmatch Spa
  */
 
@@ -32,28 +32,23 @@ StopWatch.prototype.stop = function() {
  * @param slotList Array A List of Slots
  * @param options Scrambler settings
  */
-function AdsScrambler(containers, slots, options) {
-  
+function AdsScrambler(slots, options) {
+
   /**
    * Slot performances
    */
   this.stopWatch = new StopWatch();
-  
-  /**
-   * Ads Slot list
-   */
-  this.containers = containers;
-  
+
   /**
    * Slots
    */
   this.slots = slots;
-  
+
   /**
    * Scrambler options
    */
   this.options = options;
-  
+
   /**
    * Get Slot At Index
    * @param item A slot item
@@ -63,7 +58,7 @@ function AdsScrambler(containers, slots, options) {
     for(var pos in this.slots) {
       var aSlot=this.slots[pos];
       if( aSlot['container']==item ) {
-        foundSlot=aSlot; 
+        foundSlot=aSlot;
         break;
       }
     }
@@ -72,7 +67,7 @@ function AdsScrambler(containers, slots, options) {
     }
     return foundSlot;
  };
- 
+
  /**
   * Display next slot
   * @param slot A slot item position in the slot array
@@ -81,12 +76,13 @@ function AdsScrambler(containers, slots, options) {
    var slot = this.slotAtIndex(item);
    if(slot) {
       if(typeof(googletag)!='undefined') {
-          googletag.defineSlot(aSlot['id'], aSlot['size'], aSlot['container']).addService(googletag.pubads());
-        
+        googletag.cmd.push(function() { // push slot
+          googletag.defineSlot( aSlot['id'], aSlot['size'], aSlot['unitid'] ).addService( googletag.pubads() );
+        });
       }
    }
  };
- 
+
  /**
   * Display a slot with a throttled delay
   * @param slot A slot item position in the slot array
@@ -94,55 +90,88 @@ function AdsScrambler(containers, slots, options) {
  this.displayAt = function(pos) {
    var self=this;
    AdsScrambler.delay(self, function() {
-     self.display( self.containers[pos] );   
+     //@TODO
+     //self.display( self.containers[pos] );
    }, this.options['delay'] * pos);
  }
- 
+
  /**
   * Display a Slot
   * @param el A slot item
   */
- this.display  = function(el) {   
-   googletag.cmd.push(function() { googletag.display( el ); });
+ this.display  = function(el) {
+   if(typeof(googletag)!='undefined') {
+    googletag.cmd.push( function() { googletag.display( el ); } );
+   }
  }
- 
+
  /**
   * Scramble slots in the slot queue
   */
  this.scramble = function() {
-   if( AdsScrambler.queue >= this.containers.length ) return;
    this.displayAt(AdsScrambler.queue);
    AdsScrambler.queue++;
  };//scramble
- 
+
+ /**
+  * Push all tags
+  * @param containerId Parent container
+  */
+ this.pushAllTags = function() {
+    for( var slot in this.slots) { // slots
+      var aSlot = this.slots[slot];
+      var adsContainer = document.getElementById(aSlot['containerid']);
+      if(adsContainer==null) {
+        console.error(aSlot['unitid'], "No ads container");
+        continue;
+      }
+      AdsScrambler.injectTag(
+        adsContainer,
+        aSlot['unitid'],
+        aSlot['size']
+      );
+    }//slots
+ };
+
  /**
   * Push GoogleTag Slots
   */
  this.push = function() {
-   
+
    var self=this;
-   
+
    console.log("AdsScramblers "  + self.version(), "@2015 https://musixmatch.com" );
-   
-   if( typeof(adsbygoogle)!='undefined' ) { // need to have a static lazy loading
-   
+
+   if(typeof(googletag)=='undefined') {
+    console.warn("googletag not available.");
+    return;
    }
-   
+
+   if( typeof(adsbygoogle)=='undefined' ) {
+    console.warn("adsbygoogle not available.");
+    return;
+   }
+
+   // Google Adsense
+   if( typeof(adsbygoogle)!='undefined' ) { // need to have a static lazy loading
+
+   }
+
+   // Google DFP Async commands
    googletag.cmd.push(function() {
-      
+
     // Load Slots
     for( var slot in self.slots) { // slots
       var aSlot = self.slots[slot];
-      googletag.defineSlot(aSlot['id'], aSlot['size'], aSlot['container']).addService(googletag.pubads());
-      //break; // first slot only
+      googletag.defineSlot(aSlot['id'], aSlot['size'], aSlot['unitid']).addService(googletag.pubads());
     }
-    
+
     // Google PubService settings
     googletag.pubads().collapseEmptyDivs(self.options['collapse']);
     googletag.pubads().enableSingleRequest();
-    
+
     self.stopWatch.start(); // start metrics
-    
+
     // Event Listeners
     var eventsListeners = [{
       'event' : 'slotRenderEnded',
@@ -150,13 +179,13 @@ function AdsScrambler(containers, slots, options) {
           if( typeof(adsScrambler)!='undefined' ) { // need to have a static lazy loading
             var slotId=event.slot.A;
             var slotContainer = event.slot.l.m;
-            
+
             self.stopWatch.stop();
             var elapsedTime=parseFloat(self.stopWatch.ElapsedMilliseconds);
             console.log('slotRenderEnded', slotContainer,elapsedTime);
-            
+
             adsScrambler.displayNextSlot( slotContainer );
-        
+
         } //adsScrambler
       }//listener
     },
@@ -168,44 +197,106 @@ function AdsScrambler(containers, slots, options) {
         var elapsedTime=parseFloat(self.stopWatch.ElapsedMilliseconds);
         console.log('impressionViewable', slotContainer, elapsedTime);
         if( typeof(adsScrambler)!='undefined' ) { // need to have a static lazy loading
-        
+
         }//adsScrambler
       } //listener
     }
     ];
-    
+
     // Add Event Listeners
     for( var listener in eventsListeners ) {
       var eventObj= eventsListeners[listener];
-      googletag.pubads().addEventListener( eventObj['event'], eventObj['listener']); 
+      googletag.pubads().addEventListener( eventObj['event'], eventObj['listener']);
     }
-    
+
     // Now enable all slots
     googletag.enableServices();
-    
+
     // Google Tag settings
-    googletag.setRefreshUnfilledSlots(self.options['backfill']);
-    
-    
+    googletag.companionAds().setRefreshUnfilledSlots( self.options['backfill'] );
+
+
   }); //googletag.cmd.push
-  
+
  }; //push
- 
+
  /** Ads Scrambler Version */
  this.version = function() { return "1.0.0"; }; //version
- 
+
  /** Return available DFP slots */
  this.getSlots = function() { return this.slots; }; //getSlots
- 
- /** Return  DFP containers */
- this.getContainers = function() { return this.containers; }; //getContainers
- 
+
 }; //AdsScrambler
 
 /**
  * The Ads Scrambler slot queue
  */
 AdsScrambler.queue=0;
+
+/**
+ * Debug to console
+ */
+AdsScrambler.DEBUG=1;
+
+/**
+ * Log to console
+ * @param message Message to log
+ */
+AdsScrambler.log = function(message) {
+  if(AdsScrambler.DEBUG) {
+    console.log.apply( this, Array.prototype.slice.call(arguments) );
+  }
+};
+
+/**
+  * Execute when document is ready
+  * @param callback Block to be executed
+  */
+ AdsScrambler.run = function(callback) {
+   document.addEventListener("DOMContentLoaded", function(event) {
+      callback.apply(this);
+   });
+ };
+
+/**
+ * Inject Ads Div Tag
+ * @param container Parent element
+ * @param id Tag Identifier
+ * @param size Array of sizes of form [width, height]
+ */
+AdsScrambler.injectTag  = function(container, id, sizes) {
+  var el = document.createElement("div");
+  el.setAttribute("id", id);
+  if(typeof(sizes)!='undefined') {
+    if(sizes.length>0) {
+        if( typeof(sizes[0]) == "object" ) { //array of [w,h]
+          for(var idx in sizes) {
+            var aSize = sizes[idx];
+            console.log("Unit "+id,aSize);
+          }
+        }
+        else { // [w,h]
+          var width = sizes[0];
+          var heigth = sizes[1];
+          console.log("Unit "+id,sizes);
+          el.setAttribute("style",'height:'+width+'px; width:'+heigth+'px;');
+      }
+    }
+  }
+  if(AdsScrambler.DEBUG) { // append debug and tag unit
+    var cont = document.createElement("div");
+    var dbg = document.createElement("span");
+    var sep = document.createElement("hr");
+    dbg.innerHTML = id;
+    cont.appendChild(dbg);
+    cont.appendChild(el);
+    cont.appendChild(sep);
+    container.appendChild(cont);
+  } else { // append tag unit
+     container.appendChild(el);
+  }
+
+};
 
 /**
    * Inject Adsense script
@@ -223,7 +314,7 @@ AdsScrambler.injectAdsense = function(callback) {
   })();
 (adsbygoogle = window.adsbygoogle || []).push({});
 adsbygoogle.onload = function() {
-      console.log( "adsense ready." ); 
+      console.log( "adsense ready." );
  };
 };
 
@@ -250,7 +341,7 @@ AdsScrambler.injectDFP = function(callback) {
 AdsScrambler.delay = function(self, callback, DELAY) {
   setTimeout(function() {
     callback.apply(this);
-  },DELAY,self);  
+  },DELAY,self);
 };
 
 /**
